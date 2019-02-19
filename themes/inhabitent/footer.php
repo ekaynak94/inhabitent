a2484e8f9b6c5b0ed013053058feb7a305464fac
Emre Kaynak <emrekaynak94@gmail.com>
2019-02-18 17:16:56 -0800

    Added content to header.php and footer.php

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,9 +10,28 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+			<div class="footer-contact-info">
+			  <?php echo "<h3> contact info </h3>";?>
+			  	<p><i class="fas fa-envelope"></i><a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('info@inhabitent.com')); ?></a></p>
+			  	<p><i class="fas fa-phone"></i><a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('778-456-7891')); ?></a></p>
+			  	<p>
+			  		<span><i class="fab fa-facebook-square"></i></span>
+			  		<span><i class="fab fa-twitter-square"></i></span>
+			  		<span><i class="fab fa-google-plus-square"></i></span>
+			  	</p>
+		 	</div>
+			<div class="footer-business-hours">
+				<?php echo "<h3> business hours </h3>";?>
+				<?php echo "<p><strong> Monday-Friday:</strong> 9am to 5pm</p>";?>
+				<?php echo "<p><strong> Saturday:</strong> 10am to 2pm</p>";?>
+				<?php echo "<p><strong> Sunday:</strong> Closed</p>";?>
+      		</div>
+			<div class="footer-logo">
+        		<a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-text-dark.svg"/></a>	
+      		</div>
+			<div class="footer-copyright">
+      			<p>copyright &copy; 2019 inhabitent </p>
+    		</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
