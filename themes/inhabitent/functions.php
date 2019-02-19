5cd76b800d5393a13289a3e34bce90437e2c08fa
Emre Kaynak <emrekaynak94@gmail.com>
2019-02-18 18:57:56 -0800

    Removed code editor links from admin area

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -95,6 +95,13 @@ function inhabitent_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'inhabitent_scripts' );
 
+// Remove "Editor" links from sub-menus
+function inhabitent_remove_submenus() {
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
 /**
  * Custom template tags for this theme.
  */
