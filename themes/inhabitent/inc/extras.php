263e001bc77d10cb4b6de9ae0214e31f531830ca
Emre Kaynak <emrekaynak94@gmail.com>
2019-02-18 16:37:05 -0800

    Updated login screen and enqueued fontawesome to use for footer and sidebar icons

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -20,3 +20,23 @@ function red_starter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'red_starter_body_classes' );
+
+function red_starter_login_url($url){return get_site_url();}
+add_filter('login_headerurl','red_starter_login_url');
+
+function red_starter_login_logo() {
+	$logo_url=get_template_directory_uri().'./assets/images/logos/inhabitent-logo-text-dark.svg';
+	echo '<style type="text/css">                                                                   
+	#login h1 a, .login h1 a  { background-image:url(' . $logo_url . ');
+		height:53px;		
+		width:300px;
+		background-size: 100%;
+		background-repeat: no-repeat;
+        padding-bottom: 30px;
+	}                        
+	.login .button.button-primary{
+		background-color:#248A83;
+	}    
+</style>';
+}
+add_action('login_head', 'red_starter_login_logo');
