31f247a35227837c5c59b920a042d064e5cb457c
Emre Kaynak <emrekaynak94@gmail.com>
2019-02-22 07:51:32 -0800

    Sized hero image on the front-page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,10 +8,20 @@
 get_header(); ?>
 <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+         <section style="
+         background: linear-gradient(180deg,rgba(0,0,0,.4) 0,rgba(0,0,0,.4)),#969696 url(<?php echo get_template_directory_uri().'/assets/images/home-hero.jpg'?>) no-repeat top;
+         background-size: cover,cover;
+         display:flex;
+         justify-content:center;
+         width:100%;
+         height:100vh;"
+         >
+            <img src="<?php echo get_template_directory_uri().'/assets/images/logos/inhabitent-logo-full.svg'?>">
+         </section>
          <section>
             <?php while ( have_posts() ) : the_post(); ?>
 
-               <?php get_template_part( 'template-parts/content', 'page' ); ?>
+               <?php get_template_part( 'template-parts/content', 'heropage' ); ?>
 
             <?php endwhile; // End of the loop. ?>
          </section>
@@ -19,8 +29,7 @@ get_header(); ?>
             <?php $product_types=get_terms('product_type');?>
             <?php foreach ( $product_types as $product_type ) : setup_postdata( $product_type ); ?>
                <div>
-                  <?php $icon_url= get_template_directory_uri().'/assets/images/product-type-icons/' . $product_type->slug . '.svg';?>
-                  <img src=<?php echo $icon_url?>>
+                  <img src=<?php echo get_template_directory_uri().'/assets/images/product-type-icons/' . $product_type->slug . '.svg';?>>
                   <p><?php echo $product_type->description ?></p>
                   <a href=<?php echo get_term_link($product_type)?>> <?php echo $product_type->name?> stuff</a>
                </div>
@@ -43,6 +52,7 @@ get_header(); ?>
             </div>
             <?php endforeach; wp_reset_postdata(); ?>
          </section>
+
   
 		</main><!-- #main -->
 	</div><!-- #primary -->
