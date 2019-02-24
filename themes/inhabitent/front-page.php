ec8dda510ea1d222a7b1a6eb84a0eea73f24e37a
Emre Kaynak <emrekaynak94@gmail.com>
2019-02-23 17:33:11 -0800

    Add class names for front-page html elements

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-         <section style="
+         <section class="front-page-banner" style="
          background: linear-gradient(180deg,rgba(0,0,0,.4) 0,rgba(0,0,0,.4)),#969696 url(<?php echo get_template_directory_uri().'/assets/images/home-hero.jpg'?>) no-repeat top;
          background-size: cover,cover;
          display:flex;
@@ -18,39 +18,36 @@ get_header(); ?>
          >
             <img src="<?php echo get_template_directory_uri().'/assets/images/logos/inhabitent-logo-full.svg'?>">
          </section>
-         <section>
-            <?php while ( have_posts() ) : the_post(); ?>
-
-               <?php get_template_part( 'template-parts/content', 'heropage' ); ?>
-
-            <?php endwhile; // End of the loop. ?>
-         </section>
-         <section>
-            <?php $product_types=get_terms('product_type');?>
-            <?php foreach ( $product_types as $product_type ) : setup_postdata( $product_type ); ?>
-               <div>
-                  <img src=<?php echo get_template_directory_uri().'/assets/images/product-type-icons/' . $product_type->slug . '.svg';?>>
-                  <p><?php echo $product_type->description ?></p>
-                  <a href=<?php echo get_term_link($product_type)?>> <?php echo $product_type->name?> stuff</a>
-               </div>
-            <?php endforeach; wp_reset_postdata(); ?>
-         </section>  
-         <section>
-            <?php $product_posts=inhabitent_get_latest_posts();?>
-            <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-               <div>
-                  <?php the_post_thumbnail('medium',['class'=>'thumbnail']);?>
-                  <span>
-                     <?php the_date();?>
-                     <?php echo '/';?>
-                     <?php comments_number(); ?>
-                  </span>
-                  <h3>
-                     <?php the_title();?>
-                  </h3>
-                  <a href=<?php the_permalink();?>>Read Entry</a>
+         <section class="front-page-terms">
+            <div class="container">
+               <?php $product_types=get_terms('product_type');?>
+               <?php foreach ( $product_types as $product_type ) : setup_postdata( $product_type ); ?>
+                  <div>
+                     <img src=<?php echo get_template_directory_uri().'/assets/images/product-type-icons/' . $product_type->slug . '.svg';?>>
+                     <p><?php echo $product_type->description ?></p>
+                     <a href=<?php echo get_term_link($product_type)?>> <?php echo $product_type->name?> stuff</a>
+                  </div>
+               <?php endforeach; wp_reset_postdata(); ?>
             </div>
-            <?php endforeach; wp_reset_postdata(); ?>
+         </section>  
+         <section class="front-page-posts">
+            <div class="container">
+               <?php $product_posts=inhabitent_get_latest_posts();?>
+               <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+                  <div>
+                     <?php the_post_thumbnail('medium',['class'=>'thumbnail']);?>
+                     <span>
+                        <?php the_date();?>
+                        <?php echo '/';?>
+                        <?php comments_number(); ?>
+                     </span>
+                     <h3>
+                        <?php the_title();?>
+                     </h3>
+                     <a href=<?php the_permalink();?>>Read Entry</a>
+               </div>
+               <?php endforeach; wp_reset_postdata(); ?>
+            </div>
          </section>
 
   
