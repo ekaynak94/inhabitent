9366444277315047568570a60839e0dfad92b703
Emre Kaynak <emrekaynak94@gmail.com>
2019-02-25 22:04:07 -0800

    Added search bar functionalities

--- themes/inhabitent/js/site-header.js
+++ themes/inhabitent/js/site-header.js
@@ -1,5 +1,35 @@
 const jQuery = jQuery.noConflict();
 jQuery(function() {
+  let doSearch = false;
+  //toogles search field on and off by pressing search button
+  jQuery('.site-header').on('click', 'button', function() {
+    if (doSearch) {
+      doSearch = false;
+      jQuery('.search-field').removeClass('search-field-on');
+    } else {
+      event.preventDefault();
+      if (jQuery('.search-field-on').length) {
+        jQuery('.search-field').removeClass('search-field-on');
+      } else {
+        jQuery('.search-field').addClass('search-field-on');
+      }
+      jQuery('header .search-field').focus();
+    }
+  });
+
+  //triggers search button functionality when  enter key is pressed while focused on the search field
+  jQuery(document).on('keypress', function(event) {
+    if (event.which == 13 && jQuery('.search-field').is(':focus')) {
+      doSearch = true;
+      jQuery('.search-submit').trigger('click');
+    }
+  });
+
+  //makes the search field disepear when focused away
+  jQuery('.site-header').on('focusout', '.search-field', function() {
+    jQuery('.search-field').removeClass('search-field-on');
+  });
+
   //Change header style depending on the current page
   if (jQuery('body.home').length || jQuery('body.page-template-about').length) {
     jQuery('.site-header').removeClass('sticky-header');
