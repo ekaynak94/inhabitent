2075e1483e2d740c5f00c2cd79e45cbfd7354912
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-06 20:58:38 -0800

    Refactor for js file to store reused jQuery objects in variables

--- themes/inhabitent/js/site-header.js
+++ themes/inhabitent/js/site-header.js
@@ -1,17 +1,20 @@
 const jQuery = jQuery.noConflict();
 jQuery(function() {
   let doSearch = false;
+  const searchField = '.search-field';
+  const header = '.site-header';
+
   //toogles search field on and off by pressing search button
-  jQuery('.site-header').on('click', 'button', function() {
+  jQuery(header).on('click', 'button', function() {
     if (doSearch) {
       doSearch = false;
-      jQuery('.search-field').removeClass('search-field-on');
+      jQuery(searchField).removeClass('search-field-on');
     } else {
       event.preventDefault();
       if (jQuery('.search-field-on').length) {
-        jQuery('.search-field').removeClass('search-field-on');
+        jQuery(searchField).removeClass('search-field-on');
       } else {
-        jQuery('.search-field').addClass('search-field-on');
+        jQuery(searchField).addClass('search-field-on');
       }
       jQuery('header .search-field').focus();
     }
@@ -19,16 +22,16 @@ jQuery(function() {
 
   //triggers search button functionality when  enter key is pressed while focused on the search field
   jQuery(document).on('keypress', function(event) {
-    if (event.which === 13 && jQuery('.search-field').is(':focus')) {
+    if (event.which === 13 && jQuery(searchField).is(':focus')) {
       doSearch = true;
       jQuery('.search-submit').trigger('click');
     }
   });
 
   //makes the search field disepear when focused away
-  jQuery('.site-header').on('focusout', '.search-field', function() {
-    if (jQuery('.search-field').val().length === 0) {
-      jQuery('.search-field').removeClass('search-field-on');
+  jQuery(header).on('focusout', searchField, function() {
+    if (jQuery(searchField).val().length === 0) {
+      jQuery(searchField).removeClass('search-field-on');
     }
   });
 
@@ -38,20 +41,20 @@ jQuery(function() {
     jQuery('body.page-template-about').length ||
     jQuery('body.single-adventure').length
   ) {
-    jQuery('.site-header').removeClass('sticky-header');
-    jQuery('.site-header').addClass('fixed-header');
+    jQuery(header).removeClass('sticky-header');
+    jQuery(header).addClass('fixed-header');
     jQuery(window).scroll(function() {
       let scroll = jQuery(window).scrollTop();
       if (scroll > jQuery(window).height()) {
-        jQuery('.site-header').removeClass('fixed-header');
-        jQuery('.site-header').addClass('sticky-header');
+        jQuery(header).removeClass('fixed-header');
+        jQuery(header).addClass('sticky-header');
       } else {
-        jQuery('.site-header').removeClass('sticky-header');
-        jQuery('.site-header').addClass('fixed-header');
+        jQuery(header).removeClass('sticky-header');
+        jQuery(header).addClass('fixed-header');
       }
     });
   } else {
-    jQuery('.site-header').removeClass('fixed-header');
-    jQuery('.site-header').addClass('sticky-header');
+    jQuery(header).removeClass('fixed-header');
+    jQuery(header).addClass('sticky-header');
   }
 });
